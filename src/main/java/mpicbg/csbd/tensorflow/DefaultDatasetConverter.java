bb53baccf73b1aa2b5bc37a94c1a7c95b380d9f5
Benjamin Wilhelm <benjamin.wilhelm@uni-konstanz.de>
2017-10-02 16:20:21 +0200

    Use updated Tensors API from imagej-tensorflow

--- src/main/java/mpicbg/csbd/tensorflow/DefaultDatasetConverter.java
+++ src/main/java/mpicbg/csbd/tensorflow/DefaultDatasetConverter.java
@@ -15,22 +15,23 @@ public class DefaultDatasetConverter <T extends RealType<T>> implements DatasetC
 	public RandomAccessibleInterval<FloatType> tensorToDataset(Tensor tensor, DatasetTensorBridge bridge) {
 
 		// Convert back to an image
-		RandomAccessibleInterval<FloatType> outImg = Tensors.img(tensor);
+		RandomAccessibleInterval<FloatType> outImg = Tensors.imgFloat(tensor, getMapping(bridge));
+//		RandomAccessibleInterval<FloatType> outImg = Tensors.imgDirectFloat(tensor);
 
-		// Remove dimensions in the front if the model added some
-		while (outImg.numDimensions() > bridge.getInitialInputTensorShape().numDimensions()) {
-			outImg = Views.hyperSlice(outImg, 0, 0);
-		}
+		// Remove dimensions in the front if the model added some TODO check if this is needed
+//		while (outImg.numDimensions() > bridge.getInitialInputTensorShape().numDimensions()) {
+//			outImg = Views.hyperSlice(outImg, 0, 0);
+//		}
 
 		// Create the mapping to the original dimensions
-		int[] mapping = getMapping(bridge);
-		int[] reverseMapping = new int[mapping.length];
-		for (int i = 0; i < mapping.length; i++) {
-			reverseMapping[mapping[i]] = i;
-		}
-
-		// Permute back to the original dimensions
-		outImg = permuteDimensions(outImg, reverseMapping);
+//		int[] mapping = getMapping(bridge);
+//		int[] reverseMapping = new int[mapping.length];
+//		for (int i = 0; i < mapping.length; i++) {
+//			reverseMapping[mapping[i]] = i;
+//		}
+//
+//		// Permute back to the original dimensions
+//		outImg = permuteDimensions(outImg, reverseMapping);
 		return Views.dropSingletonDimensions(outImg);
 	}
 
@@ -48,53 +49,17 @@ public class DefaultDatasetConverter <T extends RealType<T>> implements DatasetC
 
 		// Permute the dimensions according to the mapping
 		int[] mapping = getMapping(bridge);
-		im = permuteDimensions(im, mapping);
+//		im = permuteDimensions(im, mapping);
 
 		// Create the tensor
-		return Tensors.tensor(im);
+		return Tensors.tensor(im, mapping);
 	}
-	
-	// TODO maybe put in bridge?
-	private static <T> RandomAccessibleInterval<T> permuteDimensions(RandomAccessibleInterval<T> im, int[] mapping) {
-		RandomAccessibleInterval<T> output = im;
-		int[] mapped = new int[im.numDimensions()];
-		for (int i = 0; i < im.numDimensions(); i++ ) {
-			mapped[i] = i;
-		}
-		for (int i = 0; i < im.numDimensions(); i++) {
-			int from = mapping[i];
-			while (from != mapped[from]) {
-				from = mapped[from];
-			}
-			int to = i;
-			output = Views.permute(output, from, to);
-			mapped[i] = from;
-		}
-		return output;
-	}
-	
+
 	private int[] getMapping(DatasetTensorBridge bridge) {
 		int[] mapping = new int[bridge.getInitialInputTensorShape().numDimensions()];
 		for (int i = 0; i < mapping.length; i++) {
-			mapping[i] = bridge.getMapping(4 - i); // TODO that seems ugly...
+			mapping[i] = bridge.getMapping(i + 5 - bridge.getInitialInputTensorShape().numDimensions()); // TODO that seems ugly...
 		}
 		return mapping;
-	}
-	
-	// -------------------------------------------------------- DEBUG OUTPUTS
-	private static void printDim(String name, Tensor im) {
-		System.out.print(name + ": [ ");
-		for (int i = 0; i < im.shape().length; i++) {
-			System.out.print(im.shape()[i] + " ");
-		}
-		System.out.println("]");
-	}
-
-	private static void printDim(String name, RandomAccessibleInterval<?> im) {
-		System.out.print(name + ": [ ");
-		for (int i = 0; i < im.numDimensions(); i++) {
-			System.out.print(im.dimension(i) + " ");
-		}
-		System.out.println("]");
 	}
 }
