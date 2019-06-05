2836a3b5f86bf8370048b8ae09b372282f119beb
sebherbert <sherbert@pasteur.fr>
2019-06-05 12:28:42 +0200

    Browse in files to find images
    Script parameters allow for the search of files in the computer, simplifying the search

--- script/CARE_generic.py
+++ script/CARE_generic.py
@@ -1,17 +1,17 @@
-# @String(label="Input path (.tif or folder with .tifs)", required=false, value='/home/random/Development/imagej/project/CSBDeep/script') input
-# @String(label="Output path (.tif or folder)", required=false, value='/home/random/Development/imagej/project/CSBDeep/script/out') output
-# @String(label="Model file", required=false, value='/home/random/Development/imagej/project/CSBDeep/data/Tobias Boothe/models/phago_C2_no_transform_model.zip') modelFile
-# @String(label="Model file", required=false, value='phago_C2_no_transform_model') _modelName
-# @Integer(label="Number of tiles", required=false, value=8) nTiles
-# @Integer(label="Tile overlap", required=false, value=32) overlap
-# @Boolean(label="Normalize input", required=false, value=true) normalizeInput
-# @Float(label="Bottom percentile", required=false, value=3.0, stepSize=0.1) percentileBottom
-# @Float(label="Top percentile", required=false, value=99.8, stepSize=0.1) percentileTop
-# @Boolean(label="Clip", required=false, value=false) clip
-# @Boolean(label="Show progress dialog", required=false, value=true) showProgressDialog
-# @DatasetIOService io
-# @CommandService command
-# @ModuleService module
+#@ File(label="Input path (.tif or folder with .tifs)", required=false, value='/home/random/Development/imagej/project/CSBDeep/script', style="both") input
+#@ File(label="Output path (.tif or folder)", required=false, value='/home/random/Development/imagej/project/CSBDeep/script/out', style="directory") output
+#@ File(label="Model file", required=false, value='/home/random/Development/imagej/project/CSBDeep/data/Tobias Boothe/models/phago_C2_no_transform_model.zip', style="file") modelFile
+#@ String(label="Model file", required=false, value='phago_C2_no_transform_model') _modelName
+#@ Integer(label="Number of tiles", required=false, value=8) nTiles
+#@ Integer(label="Tile overlap", required=false, value=32) overlap
+#@ Boolean(label="Normalize input", required=false, value=true) normalizeInput
+#@ Float(label="Bottom percentile", required=false, value=3.0, stepSize=0.1) percentileBottom
+#@ Float(label="Top percentile", required=false, value=99.8, stepSize=0.1) percentileTop
+#@ Boolean(label="Clip", required=false, value=false) clip
+#@ Boolean(label="Show progress dialog", required=false, value=true) showProgressDialog
+#@ DatasetIOService io
+#@ CommandService command
+#@ ModuleService module
 
 from java.io import File
 import sys
@@ -37,6 +37,10 @@ def runNetwork(inputPath, outputPath):
 	myoutput = mymod.getOutput("output")
 	print(myoutput)
 	io.save(myoutput, outputPath)
+
+
+input = str(input) # change object from file to str
+output = str(output)  # change object from file to str
 
 if input.endswith(".tif"):
 	if output.endswith(".tif"):
